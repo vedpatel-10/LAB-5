9a0823b691fe96378df6ecf324fd6e45b078d1de
vedpatel-10 <24bee209@spt.pdpu.ac.in>
2025-04-20 12:55:18 +0530

    Update Q - 2.py

--- Q - 2.py
+++ Q - 2.py
@@ -7,3 +7,9 @@ print("total count :"+ str(random_numbers.count(number)))
 for i,ele in enumerate(random_numbers):
     if ele == number:
       print(i,ele)
+
+#OUTPUT:
+# [37, 46, 90, 57, 16, 27, 64, 40, 94, 47, 36, 79, 29, 87, 74, 99, 18, 36, 45, 99]
+# Enter a number: 27
+# total count :1
+# INDEX IS 5, VALUE IS 27
